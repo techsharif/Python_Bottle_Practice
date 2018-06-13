25b11da674fee62ac7b946c3a539abfbb00f33b1
techsharif <sharif.cse.hstu@gmail.com>
2018-06-14 00:40:12 +0600

    complete part 3

--- main.py
+++ main.py
@@ -1,28 +1,59 @@
 __author__ = 'Steve Cassidy'
 
 from bottle import Bottle, template, static_file
-
+from bottle import get, post, request, redirect
 app = Bottle()
 
-from interface import position_list, position_get
+from interface import position_list, position_get, position_add
 
 import sqlite3
-
+from users import check_login, session_user, generate_session, delete_session
 
 
 @app.route('/')
 def index(db):
     position_list_ = [(position[0],position[1],position[2],position[3],str(str(position[6])[:100])) for position in  position_list(db)]
+    form_data = '''
+            <form action="/login" method="post" id="loginform">
+                Nick: <input name="nick" type="text" />
+                Password: <input name="password" type="password" />
+                <input value="Login" type="submit" />
+            </form>
+        '''
+    user = session_user(db)
+    if user:
 
+        form_data = '''
+        Logged in as Bobalooba
+        <br>
+        
+
+        <form action="/post" method="post" id="postform">
+                company: <input name="company" type="text" />
+                title: <input name="title" type="text" />
+                location: <input name="location" type="text" />
+                description: <input name="description" type="text" />
+                
+                <input value="Login" type="submit" />
+            </form>
+
+            <br>
+            <form action="/logout" method="post" id="logoutform">
+               
+                <button name='logout' value="Login" type="submit" >logout </but
+            </form>
+        '''
     info = {
         'title': 'Home',
-        'position_list': position_list_
+        'position_list': position_list_,
+        'form_data': form_data,
+        'fail':''
     }
 
     return template('index', info)
 
 @app.route('/positions/<id>')
-def index(db,id):
+def position(db,id):
     data = position_get(db, id)
     info = {
         'title': 'Position Info',
@@ -32,13 +63,61 @@ def index(db,id):
     return template('position_info', info)
 
 @app.route('/about/')
-def index(db):
+def about(db):
 
     info = {
         'title': 'About'
     }
 
     return template('about', info)
+
+
+@app.route('/login')
+def login(db):
+    info = {
+        'title': 'Login'
+    }
+    return template('login', info)
+
+@app.route('/login', method='POST')
+def do_login(db):
+    nick = request.forms.get('nick')
+    password = request.forms.get('password')
+    if check_login(db, nick, password):
+        generate_session(db, nick)
+        redirect("/")
+    else:
+        form_data = '''
+            <form action="/login" method="post" id="loginform">
+                Nick: <input name="nick" type="text" />
+                Password: <input name="password" type="password" />
+                <input value="Login" type="submit" />
+            </form>
+        '''
+        info = {
+                'title': 'Home',
+                'position_list': [],
+                'form_data': form_data,
+                'fail':'Login Failed, please try again <hr>'
+            }
+
+        return template('index', info)
+
+@app.route('/logout', method='POST')
+def logout(db):
+    delete_session(db,session_user(db))
+    redirect("/")
+    
+
+@app.route('/post', method='POST')
+def post_(db):
+    usernick = session_user(db)
+    company = request.forms.get('company')
+    title = request.forms.get('title')
+    location = request.forms.get('location')
+    description = request.forms.get('description')
+    position_add(db, usernick, title, location, company, description);
+    redirect("/")
 
 @app.route('/static/<filename:path>')
 def static(filename):
--- users.py
+++ users.py
@@ -6,10 +6,22 @@ Created on Mar 26, 2012
 
 # this variable MUST be used as the name for the cookie used by this application
 COOKIE_NAME = 'sessionid'
+from database import password_hash
+import uuid
+from bottle import response, request
 
 
 def check_login(db, usernick, password):
     """returns True if password matches stored"""
+    password = password_hash(password) 
+    query = f'''SELECT * FROM users where nick='{usernick}' and password='{password}' '''
+    cursor = db.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    if rows:
+        return True
+    else:
+        return False
 
 
 def generate_session(db, usernick):
@@ -20,13 +32,57 @@ def generate_session(db, usernick):
     """
 
 
+    key = str(uuid.uuid4())
+    cur = db.cursor()
+
+    # valid user check
+    cur.execute("SELECT nick FROM users WHERE nick=?", (usernick,))
+    row = cur.fetchone()
+
+    if not row:
+        return None
+
+    # check if session already exists 
+    cur.execute("SELECT sessionid FROM sessions WHERE usernick=?", (row[0],))
+
+    row = cur.fetchone()
+
+
+    if row:
+        return row[0]
+
+    # store this new session key in the database with no likes in the value
+    cur.execute(f"INSERT INTO sessions VALUES ('{key}', '{usernick}')")
+    db.commit()
+
+    response.set_cookie(COOKIE_NAME, key)
+
+    return key
+
+
 def delete_session(db, usernick):
     """remove all session table entries for this user"""
+    cur = db.cursor()
+    cur.execute(f"DELETE FROM sessions WHERE usernick='{usernick}'")
+    db.commit()
 
 
 def session_user(db):
     """try to
     retrieve the user from the sessions table
     return usernick or None if no valid session is present"""
+
+    key = request.get_cookie(COOKIE_NAME)
+
+    cur = db.cursor()
+    cur.execute("SELECT usernick FROM sessions WHERE sessionid=?", (key,))
+
+    row = cur.fetchone()
+
+
+    if row:
+        return row[0]
+    else:
+        None
 
 
